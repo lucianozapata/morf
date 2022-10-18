b31622c872b1ce51ca10ef47204975654041a4c2
Luciano <luciano.zapata@mentimeter.com>
2022-10-18 15:17:38 +0200

    feat: just before drag and drop

--- src/presenters/StartpagePresenter.js
+++ src/presenters/StartpagePresenter.js
@@ -3,10 +3,11 @@ import MorfView from "../views/newMorfView"
 
 export default function StartpagePresenter() {
 
+    const [morfName, setMorfName] = React.useState("");
 
     return (
         <>
-        <MorfView morfNameFunction/>
+        <MorfView morfNameFunction={(name) => setMorfName(name)} morfName={morfName}/>
         </>
     )
 }
--- src/views/newMorfView.js
+++ src/views/newMorfView.js
@@ -1,39 +1,76 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 
 export default function morfView(props) {
 
-    const [morfNamn, setMorfName] = React.useState("");
-    const morfNameRef = React.useRef()
-
-    function handleMorfName(e) {
-
-        console.log(morfNameRef.current.value);
-    }
-
-
-    return (
-        <>
-            <div style={{textAlign: "center", fontSize: "100px"}}> Morfius</div>
-                <div id="flexDiv">
-                    <div className="one">
-                    <Form.Label>Skriv in namnet på Morfpersonen</Form.Label>
-                    </div>
-                <div className="one">
-                    <Form.Control
-                        ref={morfNameRef}
-                        onChange={handleMorfName}
-                    />
-                </div>
-                <div className="one">
-                    <Form.Text muted>
-                      Tänk på att skriva in stora och små bokstäver
-                    </Form.Text>
-                </div>
     
+
+    // Hanterare av det som skrivs i inputen
+    const morfNameRef = React.useRef()
+    const [splittedMorf, setSplittedMorf] = React.useState(null)
+
+  
+    return (
+
+
+        <>
+
+            {splittedMorf !== null ? (
+                
+                <>
+                <div style={{textAlign: "center", fontSize: "100px", color: "white"}}> Morfius</div>
+                <div id="morfDiv">
+                    {splittedMorf.map((name,index) => {
+                        return (
+                            <Button id="morfButtons" variant="success">{name}</Button>
+                        )
+
+                    }) }
                 </div>
+                </>
+
+
+            ) : (
+                <>
+                  <div style={{textAlign: "center", fontSize: "100px", color: "white"}}> Morfius</div>
+                    <div id="flexDiv">
+                        <div className="one" style={{color:"white"}}>
+                            <Form.Label>Skriv in namnet på Morfpersonen</Form.Label>
+                        </div>
+                        <div className="one">
+                            <Form.Control
+                                ref={morfNameRef}
+                                onChange={handleMorfName}
+                            />
+                        </div>
+                        <div className="one" style={{color:"white"}}>
+                            <Form.Text muted>
+                                Tänk på att skriva in stora och små bokstäver
+                            </Form.Text>
+                        </div>
+  
+                        <div>
+                        <Button onClick={startMorf} variant="light">Let's morf</Button>{' '}
+                        </div>
+                  </div>
+                </>
+            )}
+          
         
         </>
     )
+
+
+    function handleMorfName(e) {
+        props.morfNameFunction(morfNameRef);
+    }
+
+    function startMorf(e) {
+        let splittedMorfs = props.morfName.current.value.split("");
+        setSplittedMorf(splittedMorfs);
+    }
+    
+
 }
